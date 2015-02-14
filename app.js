a25dc1d33b5b7484083b64f2c6a75c3ffb113f21
Edouard SCHWEISGUTH <edznux@gmail.com>
2015-02-14 16:21:28 +0100

    Add views (ejs), inventory

--- app.js
+++ app.js
@@ -3,23 +3,29 @@ var steam = require('./lib/steam');
 
 var app = express();
 
+app.set ('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
-  res.send('please enter your nickname after the url of this website. like : localhost:3000/edznux');
+	res.render('index');
+  // res.send('please enter your nickname after the url of this website. like : localhost:3000/edznux');
 });
 
 app.get('/:nickname', function(req, res){
-	res.setHeader('Content-Type', 'application/json');
 	steam.getInventory(req.params.nickname,function(d){
 		var wp = steam.getWeapons(d);
 		var priceUrl = steam.getPricesUrl(d);
 		var prices;
 		steam.getPrices(priceUrl,function(data){
 			// console.log(data);
-			prices = data;
-			total = steam.totalPrice(prices)
-			res.send("Votre inventaire vaux : "+total);
+			steam.clearErrors();
+			total = steam.totalPrice(data);
+			console.log(d);
+			res.render('index',{"inventory": d, "value" : total,"err" :steam.errors});
+			// res.send("Votre inventaire vaux : "+total + " <br> Avec " + steam.errors.length + " erreur(s)" + steam.errors);
 		});
-		// res.send("prout");
+		// res.send("prout");	
 
 		// console.log(wp);
 		// console.log(priceUrl);
@@ -27,4 +33,4 @@ app.get('/:nickname', function(req, res){
 	});
 });
 
-app.listen(3000);
+app.listen(3636);
